6b96855a80fd5a62c6a1ee02fbe55dc9ff1c7154
soohoonlee <jjinimania@gmail.com>
2021-03-23 00:17:36 +0900

    meta information - beanDefinition

--- src/test/kotlin/hello/core/beandefinition/BeanDefinitionTest.kt
+++ src/test/kotlin/hello/core/beandefinition/BeanDefinitionTest.kt
@@ -1,4 +1,24 @@
 package hello.core.beandefinition
 
+import hello.core.AppConfig
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.config.BeanDefinition
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+
 class BeanDefinitionTest {
+    private val ac = AnnotationConfigApplicationContext(AppConfig::class.java)
+
+    @DisplayName(value = "빈 설정 메타정보 확인")
+    @Test
+    fun findApplicationBean() {
+        val beanDefinitionNames = ac.beanDefinitionNames
+        for (beanDefinitionName in beanDefinitionNames) {
+            val beanDefinition = ac.getBeanDefinition(beanDefinitionName)
+
+            if (beanDefinition.role == BeanDefinition.ROLE_APPLICATION) {
+                println("beanDefinitionName = $beanDefinitionName beanDefinition = $beanDefinition")
+            }
+        }
+    }
 }
